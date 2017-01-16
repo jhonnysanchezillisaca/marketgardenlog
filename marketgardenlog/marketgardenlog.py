89b8cbfb1700148f6fef604b2947cb29dcc296b2
jhonnysanchezillisaca <jhonny15993@gmail.com>
2017-01-16 20:27:18 +0100

    Added templates and forms

--- marketgardenlog/marketgardenlog.py
+++ marketgardenlog/marketgardenlog.py
@@ -8,52 +8,54 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/gardens')
 def showGardens():
-    return "List all the gardens"
+    return render_template('gardens.html')
 
 
 @app.route('/gardens/<int:garden_id>/')
 def showGarden(garden_id):
-    return "Shows a garden"
+    return render_template('garden.html', garden_id=garden_id)
 
 
 @app.route('/gardens/<int:garden_id>/edit')
 def editGarden(garden_id):
-    return "Edit garden %s" % garden_id
+    return render_template('edit_garden.html', garden_id=garden_id)
 
 
 @app.route('/gardens/<int:garden_id>/delete')
 def deleteGarden(garden_id):
-    return "Delete garden %s" % garden_id
+    return render_template('delete_garden.html', garden_id=garden_id)
 
 
 @app.route('/gardens/new')
-def newGarden(garden_id):
-    return "Create new garden"
+def newGarden():
+    return render_template('new_garden.html')
 
 
 @app.route('/gardens/<int:garden_id>/newplant')
 def newPlant(garden_id):
-    return "Create new plant"
+    return render_template('new_plant.html', garden_id=garden_id)
 
 
 @app.route('/gardens/<int:garden_id>/<int:plant_id>/edit')
 def editPlant(garden_id, plant_id):
-    return "Edit plant %s from garden %s" % (plant_id, garden_id)
+    return render_template('edit_plant.html', garden_id=garden_id,
+                           plant_id=plant_id)
 
 
 @app.route('/gardens/<int:garden_id>/<int:plant_id>/delete')
 def deletePlant(garden_id, plant_id):
-    return "Delete plant %s from garden %s" % (plant_id, garden_id)
+    return render_template('delete_plant.html', garden_id=garden_id,
+                           plant_id=plant_id)
 
 
 @app.route('/login')
 def login():
-    return "Log in"
+    return render_template('login.html')
 
 
 @app.route('/logout')
 def logout():
-    return "Log Out"
+    return redirect('/')
 
 
 if __name__ == '__main__':
